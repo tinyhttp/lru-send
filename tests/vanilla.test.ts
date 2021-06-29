edd6518774dd15ed1e2f84852c626e93bae079e9
v1rtl <pilll.pl22@gmail.com>
2021-06-29 18:32:42 +0300

    add basic tests

---
+++ tests/vanilla.test.ts
@@ -0,0 +1,31 @@
+import { suite } from 'uvu'
+import * as assert from 'uvu/assert'
+import { App } from '@tinyhttp/app'
+import { makeFetch } from 'supertest-fetch'
+import { lruSend } from '../src/index'
+import LRU from 'quick-lru'
+
+const t = suite('Vanilla (quick-lru)')
+
+t('should work with custom LRU instance', async () => {
+  const app = new App()
+  const lru = new LRU<string, unknown>({ maxSize: 5 })
+  app.use(lruSend(lru))
+
+  let r = false
+
+  app.use((_req, res) => {
+    if (r) {
+      assert.type(lru.values()[0], 'string')
+      assert.equal(lru.size, 1)
+    }
+    r = true
+    res.send('hello')
+  })
+  const server = app.listen()
+  const fetch = makeFetch(server)
+
+  await fetch('/')
+})
+
+t.run()
